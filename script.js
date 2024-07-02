7c1d03db800d517e6ae93a636157e6ab1d32b3e1
meghnapatil <30520598+meghnapatil@users.noreply.github.com>
2024-07-02 13:53:15 +0530

    added index.html , script.css and js file

---
+++ script.js
@@ -0,0 +1,88 @@
+var a = document.getElementById("hotimgs1");
+
+a.addEventListener("mouseover", () => {
+    a.style.transform = "scale(1.1)";
+    a.style.transitionDuration = "2s";
+});
+a.addEventListener("mouseout", () => {
+    a.style.transform = "scale(1)";
+    a.style.transitionDuration = "2s";
+});
+
+var b = document.getElementById("hotimgs2");
+
+b.addEventListener("mouseover", () => {
+    b.style.transform = "scale(1.1)";
+    b.style.transitionDuration = "2s";
+});
+b.addEventListener("mouseout", () => {
+    b.style.transform = "scale(1)";
+    b.style.transitionDuration = "2s";
+});
+
+var c = document.getElementById("hotimgs3");
+
+c.addEventListener("mouseover", () => {
+    c.style.transform = "scale(1.1)";
+    c.style.transitionDuration = "2s";
+});
+c.addEventListener("mouseout", () => {
+    c.style.transform = "scale(1)";
+    c.style.transitionDuration = "2s";
+});
+
+var d = document.getElementById("hotimgs4");
+
+d.addEventListener("mouseover", () => {
+    d.style.transform = "scale(1.1)";
+    d.style.transitionDuration = "2s";
+});
+d.addEventListener("mouseout", () => {
+    d.style.transform = "scale(1)";
+    d.style.transitionDuration = "2s";
+});
+
+var e = document.getElementById("hotimgs5");
+
+e.addEventListener("mouseover", () => {
+    e.style.transform = "scale(1.1)";
+    e.style.transitionDuration = "2s";
+});
+e.addEventListener("mouseout", () => {
+    e.style.transform = "scale(1)";
+    e.style.transitionDuration = "2s";
+});
+
+var f = document.getElementById("hotimgs6");
+
+f.addEventListener("mouseover", () => {
+    f.style.transform = "scale(1.1)";
+    f.style.transitionDuration = "2s";
+});
+f.addEventListener("mouseout", () => {
+    f.style.transform = "scale(1)";
+    f.style.transitionDuration = "2s";
+});
+
+var g = document.getElementById("hotimgs7");
+
+g.addEventListener("mouseover", () => {
+    g.style.transform = "scale(1.1)";
+    g.style.transitionDuration = "2s";
+});
+g.addEventListener("mouseout", () => {
+    g.style.transform = "scale(1)";
+    g.style.transitionDuration = "2s";
+});
+
+var h = document.getElementById("hotimgs8");
+
+h.addEventListener("mouseover", () => {
+    h.style.transform = "scale(1.1)";
+    h.style.transitionDuration = "2s";
+});
+h.addEventListener("mouseout", () => {
+    h.style.transform = "scale(1)";
+    h.style.transitionDuration = "2s";
+});
+
